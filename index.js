410a156deef5fd6cbf0f04fdb56c53e53d9d98de
welshman <welshman@users.noreply.github.com>
2020-06-09 07:29:06 +0100

    Update index.js
    Small Edit at moment

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ results += randomizer[Math.floor(Math.random() * randomizer.length)];
 return results;
 
 }
-if(message.content === '>nitro') return message.channel.send("https:/"+"/discord.gift/"+ randomizer(16,'0aA'));
+if(message.content === '>help') return message.channel.send("https://raw.githubusercontent.com/welshman/NitGen/master/help.txt");
+if(message.content === '>discord') return message.channel.send("https://discord.gg/SuFN7eD");
 });
